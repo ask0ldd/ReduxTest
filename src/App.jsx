da1e0f7eb6ef94bd2f38aec21ee3991a3b7bf0a3
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-05-08 17:32:03 +0200

    better payload example

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import './App.css'
 function reducerFn(state, action) { // reducer : fonction traitant une action, appelee lors d'un dispatch
   switch(action.type){
     case 'increment':
-      console.log(action.payload) // event passed as payload
-      return {count : state.count+1}
+      console.log(action.payload.target.id) // event passed as payload
+      return {clicked : action.payload.target.id, count : state.count+1}
     case 'decrement':
-      return {count : state.count-1}
+      return {...state, count : state.count-1}
     default :
       throw new Error()
   }
@@ -15,14 +15,14 @@ function reducerFn(state, action) { // reducer : fonction traitant une action, a
 
 function App() {
 
-  const [state, dispatch] = useReducer(reducerFn, {count : 0}) // dispatch an action aka send
+  const [state, dispatch] = useReducer(reducerFn, {count : 0, clicked : "none"}) // dispatch an action aka send
 
   return (
     <main>
-      <h1>counter : {state.count}</h1>
+      <h1>counter : {state.count} / clicked : {state.clicked}</h1>
       <div style={{display:'flex', justifyContent:'space-between', width:'100%'}}>
-        <button onClick={() => dispatch({type:'decrement'})}>-1</button>
-        <button onClick={(e) => dispatch({type:'increment', payload:e})}>+1</button>
+        <button id="decButton" onClick={() => dispatch({type:'decrement'})}>-1</button>
+        <button id="incButton" onClick={(e) => dispatch({type:'increment', payload:e})}>+1</button>
       </div>
 
     </main>
